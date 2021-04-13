3e01694b79d2646e95bf3d6f9ec1dca26ebd439f
Ian <Ian.Hinden@gmail.com>
2021-04-12 22:36:43 -0700

    Character faces direction they are walking now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private MyControl myControl;
+    private float movementInput;
     [SerializeField] private float speed = 4f;
 
     private void Awake()
@@ -29,9 +30,21 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        float movementInput = myControl.Player.Movement.ReadValue<float>();
+        movementInput = myControl.Player.Movement.ReadValue<float>();
         Vector3 currentPosition = transform.position;
         currentPosition.x += movementInput * speed * Time.deltaTime;
         transform.position = currentPosition;
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        if(movementInput == -1)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        } else if(movementInput == 1)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
     }
 }
